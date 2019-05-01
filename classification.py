be8870763ca683a8109cf3ef33bc253157c5f1f3
ktkhatib <42829657+ktkhatib@users.noreply.github.com>
2019-05-01 05:15:45 +0200

    fixed some shit

--- classification.py
+++ classification.py
@@ -25,7 +25,7 @@ from sklearn.neural_network import MLPClassifier
 def random_rotation(image_array: ndarray):
     # pick a random degree of rotation between 25% on the left and 25% on the right
     image_array = np.resize(image_array,(20,20))
-    random_degree = random.uniform(-25, 25)
+    random_degree = random.uniform(-90, 90)
     return np.reshape(sk.transform.rotate(image_array, random_degree),(400,))
 
 def random_noise(image_array: ndarray):
@@ -35,12 +35,15 @@ def random_noise(image_array: ndarray):
 
 def augmentation(X_train, Y_train):
     l = len(X_train)
+    X_train = np.array(X_train)
     for i in range(0, l,20):
-        X_train.append(ndarray.tolist(random_rotation(X_train[i])))
+        tmp = np.reshape(np.array(random_rotation(X_train[i])),(1,400))
+        X_train = np.append(X_train, tmp, axis = 0)
         Y_train = np.append(Y_train,Y_train[i])
         #X_train.append(ndarray.tolist(random_rotation(X_train[i])))
         #Y_train = np.append(Y_train,Y_train[i])
-        X_train.append(ndarray.tolist(random_noise(X_train[i])))
+        tmp = np.reshape(np.array(random_noise(X_train[i])),(1,400))
+        X_train = np.append(X_train, tmp, axis = 0)
         Y_train = np.append(Y_train,Y_train[i])
     return X_train, Y_train
 
@@ -72,9 +75,9 @@ def play(imageBytes):
     imageBytes = np.append(imageBytes, hogg, axis=1)
     #imageBytes = np.append(imageBytes, lbp, axis=1)
     imageBytes = StandardScaler().fit_transform(imageBytes)
-    pca = PCA(n_components=50)
-    return pca.fit_transform(imageBytes)
-    #return imageBytes
+    #pca = PCA(n_components=50)
+    #return pca.fit_transform(imageBytes)
+    return imageBytes
 
 
 def transform_data(samples):
@@ -86,14 +89,18 @@ def transform_data(samples):
     
     return primarycomp
 
-def get_data(): 
+def get_data(detection): 
     images, labels = util.import_dataset()
     imageBytes = np.array(util.flatten_image_list (images))
     
-    primarycomp = play(imageBytes)
-    X_train, Y_train, X_test, Y_test = util.train_val_split(primarycomp, labels, 0.8)
-    
-    return X_train, Y_train, X_test, Y_test
+    if detection:
+        #imageBytes, labels = augmentation(imageBytes, labels)
+        primarycomp = play(imageBytes)
+        return primarycomp, labels
+    else:
+        primarycomp = play(imageBytes)
+        X_train, Y_train, X_test, Y_test = util.train_val_split(primarycomp, labels, 0.8)
+        return X_train, Y_train, X_test, Y_test
 
 def svm_train(X_train, Y_train):
     svmC = svm.SVC(gamma='scale', probability=True)
@@ -103,10 +110,18 @@ def svm_train(X_train, Y_train):
 
 def svm_predict(svmC, X_test):
     svm_y_pred = svmC.predict(X_test)
-    #confidence = svmC.predict_log_proba(X_test)
     confidence = svmC.predict_proba(X_test)  
     return svm_y_pred, confidence
     
+def mlp_train(X_train, Y_train):
+    mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(100, 50), random_state=1)
+    mlp.fit(X_train, Y_train)
+    return mlp
+
+def mlp_predict(mlp, X_test):
+    mlp_y_pred = mlp.predict(X_test)
+    confidence = mlp.predict_proba(X_test)  
+    return mlp_y_pred, confidence
 #X_train, Y_train = augmentation(X_train, Y_train)
 #X_train = play(X_train)
 #X_test = play(X_test)
@@ -122,7 +137,7 @@ def svm_predict(svmC, X_test):
 #print("Accuracy is ", accuracy_score(Y_test,rf_y_pred)*100)
     
 def main():
-    X_train, Y_train, X_test, Y_test = get_data()
+    X_train, Y_train, X_test, Y_test = get_data(False)
     model = svm_train(X_train, Y_train)
     svm_y_pred, confidence = svm_predict(model, X_test)
     print("Accuracy is ", accuracy_score(Y_test,svm_y_pred)*100)
--- detection.py
+++ detection.py
@@ -24,8 +24,12 @@ def import_detect_images():
 def whiteness_filter(image, width, height, ratio):
     threshold = ratio*255*width*height
     im_array = np.array(image)
+    #im_array_mid = im_array[6:14][:]
+    #threshold2 = ratio*220*len(im_array_mid)*len(im_array_mid[1])
     if (im_array.sum() > threshold):
         return False #discard white image
+    #elif (im_array_mid.sum() > threshold2):
+    #    return False #discard white image
     else:
         return True #keep image
     
@@ -66,13 +70,12 @@ def iou(coordinate1, coordinate2):
     I = 0
     if (max_point[0] - min_point[0] >= 0) and (max_point[1] - min_point[1] >= 0):
         I = box_area(min_point, max_point)
-        U = box_area(box1[0:2], box1[2:4]) + box_area(box2[0:2], box2[2:4]) - I
+        U = box_area(box1[0:2], box1[2:4]) 
+    ans = I/U
     
-    iou = I/U
-    
-    return iou
+    return ans
 
-def non_max_supression(samples, coordinates, classes, scores, iou_threshold=0.5, max_boxes=10):
+def non_max_supression(samples, coordinates, classes, scores, iou_threshold=0.3, max_boxes=10):
     nms_indices = []
     # Use iou() to get the list of indices corresponding to boxes you keep    
     scores_indexes = np.array(scores).argsort().tolist()
@@ -80,17 +83,18 @@ def non_max_supression(samples, coordinates, classes, scores, iou_threshold=0.5,
     while len(scores_indexes):
         idx_max = scores_indexes.pop()
         nms_indices.append(idx_max)
-        
+        tmp = scores_indexes[:]
         for idx in scores_indexes:
-            if iou(coordinates[idx_max], coordinates[idx]) >= iou_threshold:
-                scores_indexes.remove(idx) 
-    
+            iouScore = iou(coordinates[idx_max], coordinates[idx])
+            if  iouScore >= iou_threshold:
+                tmp.remove(idx)
+        scores_indexes = tmp[:]
     # Use index arrays to select only nms_indices from scores, boxes and classes
     
     if len(nms_indices) > max_boxes:
         nms_indices = nms_indices[0:max_boxes]
     
-    
+    print(nms_indices)
     f_scores = np.array(scores)[nms_indices].tolist()
     f_classes = np.array(classes)[nms_indices].tolist()
     f_coordinates = np.array(coordinates)[nms_indices].tolist()
@@ -101,6 +105,7 @@ def non_max_supression(samples, coordinates, classes, scores, iou_threshold=0.5,
     return f_images, f_coordinates, f_classes, f_scores
 
 def plot_boxes (image, coordinates, classes, scores):
+    plt.figure()
     plt.imshow(image)
     
     class_names = []
@@ -132,48 +137,59 @@ def plot_boxes (image, coordinates, classes, scores):
     values = [legend_map[k] for k in classes_list]
     plt.legend(values, classes_list)
 
+def detect_single_image(im, model, stride, window_sizes):
+    im_w, im_h = im.size
+    samples = []
+    coordinates = [] #(left, lower, window size)
+    for w_size in window_sizes:
+        for i in range(0,(im_h - w_size + stride),stride):
+            for j in range(0,(im_w - w_size + stride),stride):
+                box = (j, i, j + w_size, i + w_size) #(left, upper, right, lower)
+                im_crop = im.crop(box)
+                #filter of white area
+                if (whiteness_filter(im_crop, w_size, w_size, 0.7)):
+                    samples.append(im_crop)
+                    coordinates.append([j,i,w_size])
+        
+    
+    
+                    
+    
+    scores = []
+    
+    
+    for i in range(0,len(samples)):
+        samples[i] = samples[i].resize((20,20))
+        
+    samples = classification.transform_data(samples)
+    classes, scores_vec = classification.svm_predict(model, samples)
+    
+    classes = [int(i) for i in classes]
+    for i in range(0,len(scores_vec)):
+        scores.append(scores_vec[i][int(classes[i])])
+    #threshold
+    samples, coordinates, classes, scores = threshold_filter(samples, coordinates, classes, scores, 0.6)
+    
+    #non-max supression
+    samples, coordinates, classes, scores = non_max_supression(samples, coordinates, classes, scores, max_boxes = 50)
+      
+    #plotting boxes
+    plot_boxes (im, coordinates, classes, scores)   
+
 #define hyperparameters stride, window_sizes[]
 stride = 2
-window_sizes = [20, 10] 
+window_sizes = [20, 20] 
 
-#import the image
+#training the svm
+X, Y = classification.get_data(True)
+model = classification.svm_train(X, Y)
+
+#import the images
 images = import_detect_images()
 
 #sliding window
-im = images[0]
-im_w, im_h = im.size
-
-samples = []
-coordinates = [] #(left, lower, window size)
-for w_size in window_sizes:
-    for i in range(0,(im_h - w_size + stride),stride):
-        for j in range(0,(im_w - w_size + stride),stride):
-            box = (j, i, j + w_size, i + w_size) #(left, upper, right, lower)
-            im_crop = im.crop(box)
-            #filter of white area
-            if (whiteness_filter(im_crop, w_size, w_size, 3/4)):
-                samples.append(im_crop)
-                coordinates.append([j,i,w_size])
-    
-
-#classify
-X_train, Y_train, X_test, Y_test = classification.get_data()
-#model = classification.svm_train(X_train, Y_train)
-                
-
-scores = []
-
-
-for i in range(0,len(samples)):
-    samples[i] = samples[i].resize((20,20))
-    
-samples = classification.transform_data(samples)
-classes, scores_vec = classification.svm_predict(model, samples)
-
-classes = [int(i) for i in classes]
-for i in range(0,len(scores_vec)):
-    scores.append(scores_vec[i][int(classes[i])])
-
+for im in images:
+    detect_single_image(im, model, stride, window_sizes)
 
 #for i in range(0,len(samples)):
 #    #resample to 20x20
@@ -183,14 +199,7 @@ for i in range(0,len(scores_vec)):
 #    #classes.append(random.randint(0,25))
 #    #scores.append(random.random())
 
-#threshold
-samples, coordinates, classes, scores = threshold_filter(samples, coordinates, classes, scores, 0.6)
 
-#non-max supression
-samples, coordinates, classes, scores = non_max_supression(samples, coordinates, classes, scores, max_boxes = 50)
-  
-#plotting boxes
-plot_boxes (im, coordinates, classes, scores)
 
     
     
